de3239236225709a17b51752a6028bd51f602702
gabomora2200 <gabomora2200@gmail.com>
2021-10-14 14:38:59 -0600

    Subiendo Clase 3

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+
+def customer(request):
+    return render(request, 'accounts/customer.html')
+
+
+'''
+--accounts
+----templates
+------accounts
+------- dasboard.html
+------- dasboard.html
+------- dasboard.html
+'''
